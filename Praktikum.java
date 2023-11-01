63fd4b892139ad3b25a8b717092d8726cb201ab1
Natali <n170879@yandex.ru>
2023-11-01 17:39:24 +0300

    Class changed and added

--- Praktikum.java
+++ Praktikum.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Praktikum {
     public static void main(String[] args) {
-        double[] expenses = new double[7]; // Должно стать полем нового класса
+        double[] expenses = new double[7]; 
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Сколько денег у вас осталось до зарплаты?");
